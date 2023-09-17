2334a77f6b707b83506a042b68c962eb132d6cc1
Cédric <ses.courteau.cedric@gmail.com>
2023-09-17 18:13:04 +0200

    ry

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
@@ -22,15 +23,27 @@ func main() {
 
 	for {
 		conn, err := s.Accept()
-
-		defer conn.Close()
-
 		if err != nil {
 			fmt.Println("Server failed to Accept ", err.Error())
 			os.Exit(1)
 		}
 
+		defer conn.Close()
+
+		buffer := make([]byte, 1024)
+		_, err = conn.Read(buffer)
+
+		if err != nil {
+			fmt.Println("Failed to read data ", err.Error())
+		}
+
+		str := strings.Split(string(buffer), "\n")
+
+		fmt.Println(str)
+
 		conn.Write([]byte("+PONG\r\n"))
+
+		/* 		To test your implementation using the official Redis CLI, you can start your server using ./spawn_redis_server.sh and then run echo -e "ping\nping" | redis-cli from your terminal. This will send two PING commands using the same connection. */
 
 	}
 
